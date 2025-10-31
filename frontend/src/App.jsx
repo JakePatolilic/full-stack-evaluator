b789a1533abec1057ca037658b8e682a2e81110b
jlpatolilic <jakepatolilic@gmail.com>
2025-10-31 14:24:30 +0800

    modified App.jsx to only redirect to Task page if user is logged in, else return to Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import Tasks from "./Tasks"
 import Register from './Components/Register.jsx';
+import Login from './Components/Login.jsx';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>📝 React Task Evaluator</h1>
-      <Register />
-    </div>
-  );
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const userId = localStorage.getItem("userId");
+        if (userId) setIsLoggedIn(true);
+    }, []);
+
+    const handleLoginSuccess = () => setIsLoggedIn(true);
+
+    return (
+        <div className="app">
+        <h1>📝 React Task Evaluator</h1>
+        {isLoggedIn ? <Tasks /> : <Login onLoginSuccess={handleLoginSuccess} />}
+        </div>
+    );
 }
 
 export default App
